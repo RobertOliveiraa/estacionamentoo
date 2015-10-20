c18a8df09566904eede83a3d3139e505dd463147
Robert Pinto de Oliveira <robert_oliveiraa@icloud.com>
2015-10-19 21:49:32 -0400

    Criacao de conexao com o banco na classe connectionfactory

--- Estacionar/src/model/connection/ConnectionFactory.java
+++ Estacionar/src/model/connection/ConnectionFactory.java
@@ -5,10 +5,33 @@
  */
 package model.connection;
 
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author RobertOliveira
  */
 public class ConnectionFactory {
+    
+    public static Connection getConnection() {
+        Connection con=null;
+        ResultSet rs;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String url="jdbc:mysql://localhost:8889/parking";
+            String usuario="root";
+            String senha="root";
+            con=DriverManager.getConnection(url,usuario,senha);
+            JOptionPane.showMessageDialog(null, "Conectado");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException | HeadlessException erro) {
+            JOptionPane.showMessageDialog(null, "ERRO DE JDBC"+erro.getMessage());
+        }
+        return con;
+    }
     
 }
