7555bd729b337307678b1095f33695a4268660b5
Robert Pinto de Oliveira <robert_oliveiraa@icloud.com>
2015-10-19 22:26:39 -0400

    Inicializacao da ReservaDao

--- Estacionar/src/model/dao/ReservaDao.java
+++ Estacionar/src/model/dao/ReservaDao.java
@@ -5,10 +5,38 @@
  */
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import model.bean.Reserva;
+import model.connection.ConnectionFactory;
+
 /**
  *
  * @author RobertOliveira
  */
 public class ReservaDao {
+    
+    Connection con=ConnectionFactory.getConnection();
+    Statement stm;
+    PreparedStatement pstm;
+    ResultSet rs;
+    String sql;
+    
+    public void salvarReserva(Reserva reserva){
+        try{
+            sql="insert into aluguel (aluclicod,aluautcod,aludtdevolucao,aludtprevisaodevol,aludtaluguel,aluqtddias,aluvalor)values(?,?,?,?,?,?,?)";
+            pstm=con.prepareStatement(sql);
+            
+            pstm.execute();
+            
+            
+            JOptionPane.showMessageDialog(null, "Dados salvos Com Sucesso");
+        }catch(Exception erro){
+            JOptionPane.showMessageDialog(null, "Dados nao Salvo\n"+erro.getMessage());
+        }
+    }
     
 }
