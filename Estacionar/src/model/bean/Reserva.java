eb73e8c536025fa655413602559b5fab21529658
Robert Pinto de Oliveira <robert_oliveiraa@icloud.com>
2015-10-19 22:08:45 -0400

    Adicionamento variaveis na classe Reservar na model.bean

--- Estacionar/src/model/bean/Reserva.java
+++ Estacionar/src/model/bean/Reserva.java
@@ -11,4 +11,100 @@ package model.bean;
  */
 public class Reserva {
     
+    private Integer rescod;
+    private Carro carro;
+    private Cliente cliente;
+    private Double resvalor;
+    private String resdthrent;
+    private String resdthrsai;
+    private String resdthrdur;
+    private Double respagamento;
+    private Double restroco;
+    private String resestatus;
+    
+    public Reserva(){
+        
+    }
+
+    public Integer getRescod() {
+        return rescod;
+    }
+
+    public void setRescod(Integer rescod) {
+        this.rescod = rescod;
+    }
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Double getResvalor() {
+        return resvalor;
+    }
+
+    public void setResvalor(Double resvalor) {
+        this.resvalor = resvalor;
+    }
+
+    public String getResdthrent() {
+        return resdthrent;
+    }
+
+    public void setResdthrent(String resdthrent) {
+        this.resdthrent = resdthrent;
+    }
+
+    public String getResdthrsai() {
+        return resdthrsai;
+    }
+
+    public void setResdthrsai(String resdthrsai) {
+        this.resdthrsai = resdthrsai;
+    }
+
+    public String getResdthrdur() {
+        return resdthrdur;
+    }
+
+    public void setResdthrdur(String resdthrdur) {
+        this.resdthrdur = resdthrdur;
+    }
+
+    public Double getRespagamento() {
+        return respagamento;
+    }
+
+    public void setRespagamento(Double respagamento) {
+        this.respagamento = respagamento;
+    }
+
+    public Double getRestroco() {
+        return restroco;
+    }
+
+    public void setRestroco(Double restroco) {
+        this.restroco = restroco;
+    }
+
+    public String getResestatus() {
+        return resestatus;
+    }
+
+    public void setResestatus(String resestatus) {
+        this.resestatus = resestatus;
+    }
+    
+
 }
